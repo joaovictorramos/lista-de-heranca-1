68526eaf6834b198aabb23be5e06ea3846afe58b
João Ramos <97909091+joaovictorramos@users.noreply.github.com>
2022-06-13 12:54:19 -0300

    Remoção

--- SuperKate/src/main/java/superkate/Personagem.java
+++
@@ -1,9 +0,0 @@
-package superkate;
-
-public abstract class Personagem {
-    protected int vida;
-    protected int energia;
-    
-    protected abstract String pular();
-    protected abstract String andar();
-}
